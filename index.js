c5b1f5f2c52e83ae915b49da618720b5041a96c4
Joan D <joandidachr@gmail.comm>
2022-12-22 16:04:55 +0100

    Solved lab

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
+const { updateOne } = require("./models/Recipe.model");
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
@@ -13,24 +14,14 @@ mongoose
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.create({ title: "Catalan Ribs and Chickpeas Stew",
-    level: "Amateur Chef",
-    ingredients: [
-      "10 Pork Ribs",
-      "3 Red bell Peppers",
-      "3 Onions",
-      "1/4 Andalusian Olive oil",
-      "3 garlic gloves",
-      "3 tablespoons pepper",
-      "salt to taste",
-      "1 spoon of Paprika de la Vera",
-      "500gr Tomatoe Sauce",
-      "2 cloves",
-      "Tea Spoon of Cummin"
-    ],
-     });
-  })
-  .then(createdRecipe => console.log(createdRecipe)) 
+    // return Recipe.insertMany(data)
+    // return Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: "120"})
+    // console.log('Duration time updated')
+    // return Recipe.deleteOne({title: "Carrot Cake"})
+    // console.log('Carrot Cake deleted! 💩');
+     }) 
+     
+  // .then(createdRecipe => console.log(createdRecipe)) 
     
     .then(() => mongoose.connection.close())
     // Run your code here, after you have insured that the connection was made
